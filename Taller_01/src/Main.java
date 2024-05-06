b6f2ad48228b3a50d8bcacd489e19e5f8777138c
Enzo Lopez <92378273+EnzoL1016@users.noreply.github.com>
2024-05-06 20:37:22 -0300

    Taller 01 POO Java

--- Taller_01/src/Main.java
+++ Taller_01/src/Main.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     static Scanner leer = new Scanner(System.in);
     public static void main(String[] args) {
